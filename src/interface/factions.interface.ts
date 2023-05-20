08feec45c4983808e0f763455c13e66ce5ecc342
Emanuele Moncada <em@zerowk.it>
2023-05-20 11:33:26 +0200

    feat(factions): add faction model & interface

--- src/interface/factions.interface.ts
+++ src/interface/factions.interface.ts
@@ -1,3 +1,9 @@
-interface IFaction {}
+interface IFaction {
+    name: string;
+    description: string;
+    planet: string[];
+    weapons: string[];
+    technology: string[];
+}
 
 export default IFaction;
--- src/model/factions.model.ts
+++ src/model/factions.model.ts
@@ -2,7 +2,13 @@ import IFaction from "../interface/factions.interface";
 import { model, Schema } from "mongoose";
 
 const FactionSchema = new Schema<IFaction>(
-  {},
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    planet: [{ type: String, required: false }],
+    weapons: [{ type: String, required: false }],
+    technology: [{ type: String, required: false }],
+  },
   { timestamps: true, versionKey: false }
 );
 
