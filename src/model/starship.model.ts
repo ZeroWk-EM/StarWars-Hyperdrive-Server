7145592f926de459a20a6b461f84e084d950d8c5
Emanuele Moncada <em@zerowk.it>
2023-05-20 12:07:44 +0200

    feat(starship): add starship model

---
+++ src/model/starship.model.ts
@@ -0,0 +1,30 @@
+import { VechicleType } from "../interface/enum/vehicletype.enum";
+import IVehicles from "../interface/vehicles.interface";
+import { model, Schema } from "mongoose";
+
+const VehicleSchema = new Schema<IVehicles>(
+  {
+    name: { type: String, required: true },
+    model: { type: String, required: true },
+    type: {
+      type: String,
+      enum: VechicleType,
+      message:
+        "{VALUE} is not supported, see the documentation for see acceptable values",
+      required: true,
+    },
+    manufacturer: { type: String, required: false },
+    cost_in_credits: { type: Number, required: true },
+    length: { type: Number, required: false },
+    crew: { type: Number, required: false },
+    passengers: { type: Number, required: false },
+    cargo_capacity: { type: Number, required: false },
+    vehicle_class: { type: Number, required: false },
+    max_atmosphering_speed: { type: Number, required: false },
+    hyperdrive_rating: { type: Number, min: 0, max: 2, required: false },
+    MGLT: { type: Number, required: false },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export default model<IVehicles>("vehicles", VehicleSchema);
