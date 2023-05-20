fc008fb62a9fa98c4c0d5a48e45ce325aa8c0a78
Emanuele Moncada <em@zerowk.it>
2023-05-20 12:07:29 +0200

    feat(weapons): add weapons interface & model

--- src/interface/weapons.interface.ts
+++ src/interface/weapons.interface.ts
@@ -1,3 +1,10 @@
-interface IWeapons {}
+import { WeaponType } from "./enum/weapontype.enum";
+
+interface IWeapons {
+  name: string;
+  type: WeaponType;
+  description: string;
+  owner: string;
+}
 
 export default IWeapons;
--- src/model/weapons.model.ts
+++ src/model/weapons.model.ts
@@ -1,8 +1,20 @@
+import { WeaponType } from "../interface/enum/weapontype.enum";
 import IWeapons from "../interface/weapons.interface";
 import { model, Schema } from "mongoose";
 
 const WeaponsSchema = new Schema<IWeapons>(
-  {},
+  {
+    name: { type: String, required: true },
+    type: {
+      type: String,
+      enum: WeaponType,
+      message:
+        "{VALUE} is not supported, see the documentation for see acceptable values",
+      required: true,
+    },
+    description: { type: String, required: true },
+    owner: { type: String, required: true },
+  },
   { timestamps: true, versionKey: false }
 );
 
