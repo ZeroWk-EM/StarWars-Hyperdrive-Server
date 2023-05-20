553ffb09ff738819fd5e08b774e61b93b8f851f6
Emanuele Moncada <em@zerowk.it>
2023-05-20 18:56:53 +0200

    feat(routes): add character route

--- src/routes/characters.routes.ts
+++ src/routes/characters.routes.ts
@@ -1,13 +1,23 @@
 import express from "express";
 import {
   checkInfo,
+  createCharacter,
+  deleteCharacter,
   getAllCharacters,
+  getCharacterByID,
+  updateCharacter,
 } from "../controller/characters.controller";
+import { checkIdValid } from "../middleware/validation.middleware";
+import { checkBodyValid } from "../middleware/character.middleware";
 
 const router = express.Router();
 router.use(express.json());
 
 router.get("/", getAllCharacters);
+router.get("/:id", checkIdValid, getCharacterByID);
+router.post("/", checkBodyValid, createCharacter);
+router.put("/:id", checkIdValid, updateCharacter);
+router.delete("/:id", checkIdValid, deleteCharacter);
 router.get("/info", checkInfo);
 
 export default router;
