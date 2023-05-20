b4c32265b1a8ea712b7aa101bd58c5dd6ca16906
Emanuele Moncada <em@zerowk.it>
2023-05-20 12:00:00 +0200

    feat(specie): add species interface e model

--- src/interface/species.interface.ts
+++ src/interface/species.interface.ts
@@ -1,3 +1,13 @@
-interface ISpecie {}
+import { Designation } from "./enum/designation.enum";
+
+interface ISpecie {
+    name: string;
+    designation: Designation;
+    average_height: number;
+    skin_color: string;
+    average_lifespan: number;
+    homeworld: string;
+    language: string[];
+}
 
 export default ISpecie;
--- src/model/species.model.ts
+++ src/model/species.model.ts
@@ -1,8 +1,24 @@
+import { Designation } from "../interface/enum/designation.enum";
 import ISpecie from "../interface/species.interface";
 import { model, Schema } from "mongoose";
 
 const SpecieSchema = new Schema<ISpecie>(
-  {},
+  {
+    name: { type: String, required: true },
+    designation: {
+      type: String,
+      enum: Designation,
+      message:
+        "{VALUE} is not supported, see the documentation for see acceptable values",
+
+      required: true,
+    },
+    average_height: { type: Number, required: true },
+    skin_color: { type: String, required: true },
+    average_lifespan: { type: Number, required: true },
+    homeworld: { type: String, required: true },
+    language: [{ type: String, required: true }],
+  },
   { timestamps: true, versionKey: false }
 );
 
