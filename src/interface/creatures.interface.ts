2fbdba0653ac1ecd86e60ee953bb2a3ef1ab2926
Emanuele Moncada <em@zerowk.it>
2023-05-20 11:22:14 +0200

    feat(creatures): add creature interface & model

--- src/interface/creatures.interface.ts
+++ src/interface/creatures.interface.ts
@@ -1,3 +1,15 @@
-interface ICreature {}
+import { Designation } from "./enum/designation.enum";
+import { EyeColor } from "./enum/eye_color.enum";
+
+interface ICreature {
+  name: string;
+  description: string;
+  designation: Designation;
+  height: number;
+  weight: number;
+  skin_color: string;
+  eye_color: EyeColor;
+  location: string[];
+}
 
 export default ICreature;
--- src/model/creatures.model.ts
+++ src/model/creatures.model.ts
@@ -1,8 +1,31 @@
 import ICreature from "../interface/creatures.interface";
 import { model, Schema } from "mongoose";
+import { Designation } from "../interface/enum/designation.enum";
+import { EyeColor } from "../interface/enum/eye_color.enum";
 
 const CreatureSchema = new Schema<ICreature>(
-  {},
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    designation: {
+      type: String,
+      enum: Designation,
+      message:
+        "{VALUE} is not supported, see the documentation for see acceptable values",
+      required: true,
+    },
+    height: { type: Number, required: true },
+    weight: { type: Number, required: true },
+    skin_color: { type: String, required: true },
+    eye_color: {
+      type: String,
+      enum: EyeColor,
+      message:
+        "{VALUE} is not supported, see the documentation for see acceptable values",
+      required: true,
+    },
+    location: [{ type: String, required: false }],
+  },
   { timestamps: true, versionKey: false }
 );
 
