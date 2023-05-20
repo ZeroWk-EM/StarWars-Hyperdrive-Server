18d4b86200ad7f6e51838920de553082889ec508
Emanuele Moncada <em@zerowk.it>
2023-05-20 12:01:01 +0200

    feat(vehicle): add vehicles interface & model

--- src/interface/vehicles.interface.ts
+++ src/interface/vehicles.interface.ts
@@ -1,3 +1,19 @@
-interface IVehicles {}
+import { VechicleType } from "./enum/vehicletype.enum";
+
+interface IVehicles {
+  name: string;
+  model: string;
+  type: VechicleType;
+  manufacturer: string;
+  cost_in_credits: number;
+  length: number;
+  crew: number;
+  passengers: number;
+  cargo_capacity: number;
+  vehicle_class: number;
+  max_atmosphering_speed?: number;
+  hyperdrive_rating?: number;
+  MGLT?: number;
+}
 
 export default IVehicles;
--- src/model/vehicles.model.ts
+++ src/model/vehicles.model.ts
@@ -1,9 +1,27 @@
+import { VechicleType } from "../interface/enum/vehicletype.enum";
 import IVehicles from "../interface/vehicles.interface";
 import { model, Schema } from "mongoose";
 
 const VehicleSchema = new Schema<IVehicles>(
-  {},
+  {
+    name: { type: String, required: true },
+    model: { type: String, required: true },
+    type: {
+      type: String,
+      enum: VechicleType,
+      message:
+        "{VALUE} is not supported, see the documentation for see acceptable values",
+      required: true,
+    },
+    manufacturer: { type: String, required: false },
+    cost_in_credits: { type: Number, required: true },
+    length: { type: Number, required: false },
+    crew: { type: Number, required: false },
+    passengers: { type: Number, required: false },
+    cargo_capacity: { type: Number, required: false },
+    vehicle_class: { type: Number, required: false },
+  },
   { timestamps: true, versionKey: false }
 );
 
-export default model<IVehicles>("vehicles",VehicleSchema);
+export default model<IVehicles>("vehicles", VehicleSchema);
