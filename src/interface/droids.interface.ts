9b72f8c60ae8c472beb13788018b18608ce6a630
Emanuele Moncada <em@zerowk.it>
2023-05-20 11:30:58 +0200

    feat(droid): add droid interface & model

--- src/interface/droids.interface.ts
+++ src/interface/droids.interface.ts
@@ -1,3 +1,13 @@
-interface IDroid {}
+import { TypeDroid } from "./enum/droidtype.enum";
+
+interface IDroid {
+  name: string;
+  type: TypeDroid;
+  height: number;
+  weight: number;
+  owner: string;
+  factions: string[];
+  weapons: string[];
+}
 
 export default IDroid;
--- src/model/droids.model.ts
+++ src/model/droids.model.ts
@@ -1,8 +1,23 @@
 import IDroid from "../interface/droids.interface";
 import { model, Schema } from "mongoose";
+import { TypeDroid } from "../interface/enum/droidtype.enum";
 
 const DroidSchema = new Schema<IDroid>(
-  {},
+  {
+    name: { type: String, required: true },
+    type: {
+      type: String,
+      enum: TypeDroid,
+      message:
+        "{VALUE} is not supported, see the documentation for see acceptable values",
+      required: true,
+    },
+    height: { type: Number, required: false },
+    weight: { type: Number, required: false },
+    owner: { type: String, required: false },
+    factions: [{ type: String, required: true }],
+    weapons: [{ type: String, required: false }],
+  },
   { timestamps: true, versionKey: false }
 );
 
