4f6aeb18320ffff0f1df2bfd67459cafdef393b0
Emanuele Moncada <em@zerowk.it>
2023-05-20 10:52:43 +0200

    fix(character): update interface & model

--- src/interface/characters.interface.ts
+++ src/interface/characters.interface.ts
@@ -1,5 +1,29 @@
 interface ICharacter {
-    name: string;
+  name: string;
+  surname: string;
+  height: number;
+  weight: number;
+  gender: "Male" | "Female" | "Asexual" | "Unknown" | "Other";
+  hair_color: string;
+  skin_color: string;
+  eye_color:
+    | "Brown"
+    | "Amber"
+    | "Hazel"
+    | "Green"
+    | "Blue"
+    | "Gray"
+    | "Yellow"
+    | "Red"
+    | "Unknown";
+  birth_year: string;
+  homeworld: string;
+  factions: string[];
+  movies: string[];
+  series: string[];
+  specie: string;
+  vehicles: string[];
+  weapons: string[];
 }
 
 export default ICharacter;
--- src/model/characters.model.ts
+++ src/model/characters.model.ts
@@ -2,7 +2,50 @@ import ICharacter from "../interface/characters.interface";
 import { model, Schema } from "mongoose";
 
 const CharacterSchema = new Schema<ICharacter>(
-  { name: { type: String, require: true } },
+  {
+    name: { type: String, required: true },
+    surname: { type: String, required: false },
+    height: { type: Number, required: true },
+    weight: { type: Number, required: true },
+    gender: {
+      type: String,
+      enum: {
+        values: ["Male", "Female", "Asexual", "Unknown", "Other"],
+        message:
+          "{VALUE} is not supported, see the documentation for see acceptable values",
+      },
+      required: true,
+    },
+    hair_color: { type: String, required: false },
+    skin_color: { type: String, required: true },
+    eye_color: {
+      type: String,
+      enum: {
+        values: [
+          "Brown",
+          "Amber",
+          "Hazel",
+          "Green",
+          "Blue",
+          "Gray",
+          "Yellow",
+          "Red",
+          "Unknown",
+        ],
+        message:
+          "{VALUE} is not supported, see the documentation for see acceptable values",
+      },
+      required: true,
+    },
+    birth_year: { type: String, required: false },
+    homeworld: { type: String, required: false },
+    factions: [{ type: String, require: false }],
+    movies: [{ type: String, required: false }],
+    series: [{ type: String, required: false }],
+    specie: { type: String, required: true },
+    vehicles: [{ type: String, required: false }],
+    weapons: [{ type: String, required: false }],
+  },
   { timestamps: true, versionKey: false }
 );
 
