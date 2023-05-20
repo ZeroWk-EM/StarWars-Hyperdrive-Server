5ba583ebaa7c2a300cb80a994e2c397e1b0c8a81
Emanuele Moncada <em@zerowk.it>
2023-05-20 11:59:19 +0200

    feat(movie): add movies interface & model

--- src/interface/movies.interface.ts
+++ src/interface/movies.interface.ts
@@ -1,3 +1,13 @@
-interface IMovie {}
+interface IMovie {
+  title: string;
+  episode_id: number;
+  opening_crawl: string;
+  director: string;
+  producer: string;
+  release_date: Date;
+  characters: string[];
+  droid: string[];
+  vehicles: string[];
+}
 
 export default IMovie;
--- src/model/movies.model.ts
+++ src/model/movies.model.ts
@@ -2,7 +2,17 @@ import IMovie from "../interface/movies.interface";
 import { model, Schema } from "mongoose";
 
 const MovieSchema = new Schema<IMovie>(
-  {},
+  {
+    title: { type: String, require: true },
+    episode_id: { type: Number, required: true },
+    opening_crawl: { type: String, required: true },
+    director: { type: String, required: true },
+    producer: { type: String, required: true },
+    release_date: { type: Date, required: true },
+    characters: [{ type: String, required: false }],
+    droid: [{ type: String, required: false }],
+    vehicles: [{ type: String, required: false }],
+  },
   { timestamps: true, versionKey: false }
 );
 
