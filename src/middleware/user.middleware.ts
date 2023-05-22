de7fc09deff52b5d9c34a6e6b3099ab481dc1217
Emanuele Moncada <em@zerowk.it>
2023-05-22 16:03:53 +0200

    feat(middleware): add route validation middleware

---
+++ src/middleware/user.middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import User from "../model/user.model";
+
+export const checkBodyValid = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // Take key into che user schema
+  const validKeys = Object.keys(User.schema.obj);
+
+  // Take key into the body
+  const bodyKeys = Object.keys(req.body);
+
+  // Compare validKeys and bodyKeys
+  const invalidKeys = bodyKeys.filter((key) => !validKeys.includes(key));
+
+  // Se sono presenti chiavi non valide, restituisci un errore
+  if (invalidKeys.length) {
+    const errorMsg = `Invalid key(s) found in request body: ${invalidKeys.join(
+      ", "
+    )}`;
+    return res.status(400).json({ error_message: errorMsg });
+  }
+  next();
+};
