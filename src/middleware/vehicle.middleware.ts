836e30d08288deb161087225ba07c004848ba52f
wkasper <em@zerowk.it>
2023-05-23 10:02:21 +0200

    fix(middleware): remove comment

--- src/middleware/vehicle.middleware.ts
+++ src/middleware/vehicle.middleware.ts
@@ -6,16 +6,9 @@ export const checkBodyValid = (
   res: Response,
   next: NextFunction
 ) => {
-  // Take key into che user schema
   const validKeys = Object.keys(Vehicles.schema.obj);
-
-  // Take key into the body
   const bodyKeys = Object.keys(req.body);
-
-  // Compare validKeys and bodyKeys
   const invalidKeys = bodyKeys.filter((key) => !validKeys.includes(key));
-
-  // Se sono presenti chiavi non valide, restituisci un errore
   if (invalidKeys.length) {
     const errorMsg = `Invalid key(s) found in request body: ${invalidKeys.join(
       ", "
