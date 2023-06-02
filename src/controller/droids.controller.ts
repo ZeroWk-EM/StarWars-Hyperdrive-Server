5e1f22b79bb176bbb13c04191be99da85f6275e9
zerowk <em@zerowk.it>
2023-06-02 14:32:03 +0200

    feat(controller): add pagination

--- src/controller/droids.controller.ts
+++ src/controller/droids.controller.ts
@@ -3,10 +3,70 @@ import Droid from "../model/droids.model";
 import Endpoint from "../model/endpoint.model";
 import IDroid from "../interface/droids.interface";
 
-export const getAllDroids = async ({ query }: Request, res: Response) => {
+export const getAllDroids = async (req: Request, res: Response) => {
+  //PAGINATION
+  let urlPrev = null;
+  let urlNext = null;
+  const documentForPage: number = 20;
   try {
-    const getAll = await Droid.find(query);
-    if (getAll) return res.status(200).json({ status: 200, droids: getAll });
+    const totalDroid = await Droid.countDocuments({});
+    const maxpage = Math.ceil(totalDroid / documentForPage);
+    const pageNumber = Number(req.query.page);
+
+    // Se non ci sono personaggi nella collection
+    if (totalDroid === 0) {
+      return res.status(200).json({ message: "NO DROID INTO COLLECTION" });
+    }
+
+    // Se non è settato il query params page
+    if (!pageNumber) {
+      urlNext = `http://localhost:${process.env.PORT}/v1/droids?page=2`;
+    }
+
+    // Se esiste il query params page ed è maggiore o uguale ad 1
+    if (pageNumber && pageNumber >= 1) {
+      urlNext = `http://localhost:${process.env.PORT}/v1/droids?page=${
+        pageNumber + 1
+      }`;
+      // Se esiste il query params page è maggiore o uguale ad 2
+      if (pageNumber >= 2) {
+        urlPrev = `http://localhost:${process.env.PORT}/v1/droids?page=${
+          pageNumber - 1
+        }`;
+      }
+    }
+
+    // Se il numero attuale di pagione e uguale al numero massimo di pagine
+    if (pageNumber === maxpage) {
+      urlNext = null;
+      // Se si prova a mettere un numero manualemente come query param di page
+    } else if (pageNumber > maxpage) {
+      return res.status(200).json({
+        message: "No more character",
+        urlPrev:
+          (urlPrev = `http://localhost:${process.env.PORT}/v1/droids?page=${maxpage}`),
+      });
+    }
+
+    // Se il numero massimo di pagine è 1 mette a null entrambe (MESSO ALLA FINE IN MODO DA SOVVRASCRIVERE LE ALTRE COSE)
+    if (maxpage === 1) {
+      urlPrev = null;
+      urlNext = null;
+    }
+
+    const getAll = await Droid.find(req.query.name ? { name: req.query.name } : {})
+      .skip(
+        !pageNumber || pageNumber === 1 ? 0 : (pageNumber - 1) * documentForPage
+      )
+      .limit(documentForPage);
+    if (getAll)
+      return res.status(200).json({
+        maxpage,
+        totalDroid,
+        next: urlNext,
+        prev: urlPrev,
+        droids: getAll,
+      });
     res.status(404).json({ status: 404, message: "Don't exist droids list" });
   } catch (error) {
     return res.status(400).json({ status: 400, error_message: error });
--- src/controller/factions.controller.ts
+++ src/controller/factions.controller.ts
@@ -3,10 +3,72 @@ import Faction from "../model/factions.model";
 import Endpoint from "../model/endpoint.model";
 import IFaction from "../interface/factions.interface";
 
-export const getAllFactions = async ({ query }: Request, res: Response) => {
+export const getAllFactions = async (req: Request, res: Response) => {
+  //PAGINATION
+  let urlPrev = null;
+  let urlNext = null;
+  const documentForPage: number = 20;
   try {
-    const getAll = await Faction.find(query);
-    if (getAll) return res.status(200).json({ status: 200, factions: getAll });
+    const totalFaction = await Faction.countDocuments({});
+    const maxpage = Math.ceil(totalFaction / documentForPage);
+    const pageNumber = Number(req.query.page);
+
+    // Se non ci sono personaggi nella collection
+    if (totalFaction === 0) {
+      return res.status(200).json({ message: "NO FACTION INTO COLLECTION" });
+    }
+
+    // Se non è settato il query params page
+    if (!pageNumber) {
+      urlNext = `http://localhost:${process.env.PORT}/v1/factions?page=2`;
+    }
+
+    // Se esiste il query params page ed è maggiore o uguale ad 1
+    if (pageNumber && pageNumber >= 1) {
+      urlNext = `http://localhost:${process.env.PORT}/v1/factions?page=${
+        pageNumber + 1
+      }`;
+      // Se esiste il query params page è maggiore o uguale ad 2
+      if (pageNumber >= 2) {
+        urlPrev = `http://localhost:${process.env.PORT}/v1/factions?page=${
+          pageNumber - 1
+        }`;
+      }
+    }
+
+    // Se il numero attuale di pagione e uguale al numero massimo di pagine
+    if (pageNumber === maxpage) {
+      urlNext = null;
+      // Se si prova a mettere un numero manualemente come query param di page
+    } else if (pageNumber > maxpage) {
+      return res.status(200).json({
+        message: "No more character",
+        urlPrev:
+          (urlPrev = `http://localhost:${process.env.PORT}/v1/factions?page=${maxpage}`),
+      });
+    }
+
+    // Se il numero massimo di pagine è 1 mette a null entrambe (MESSO ALLA FINE IN MODO DA SOVVRASCRIVERE LE ALTRE COSE)
+    if (maxpage === 1) {
+      urlPrev = null;
+      urlNext = null;
+    }
+
+    const getAll = await Faction.find(
+      req.query.name ? { name: req.query.name } : {}
+    )
+      .skip(
+        !pageNumber || pageNumber === 1 ? 0 : (pageNumber - 1) * documentForPage
+      )
+      .limit(documentForPage);
+    if (getAll)
+      return res.status(200).json({
+        maxpage,
+        totalFaction,
+        next: urlNext,
+        prev: urlPrev,
+        factions: getAll,
+      });
     res.status(404).json({ status: 404, message: "Don't exist factions list" });
   } catch (error) {
     return res.status(400).json({ status: 400, error_message: error });
