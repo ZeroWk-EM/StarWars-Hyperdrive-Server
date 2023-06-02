5e75013c98d686e9630d07ce0f0e6f55a3103c82
zerowk <em@zerowk.it>
2023-06-02 14:41:02 +0200

    feat(controller): add pagination

--- src/controller/vehicles.controller.ts
+++ src/controller/vehicles.controller.ts
@@ -3,10 +3,74 @@ import Vehicles from "../model/vehicles.model";
 import Endpoint from "../model/endpoint.model";
 import IVehicles from "../interface/vehicles.interface";
 
-export const getAllVehicles = async ({ query }: Request, res: Response) => {
+export const getAllVehicles = async (req: Request, res: Response) => {
+  //PAGINATION
+  let urlPrev = null;
+  let urlNext = null;
+  const documentForPage: number = 20;
   try {
-    const getAll = await Vehicles.find(query);
-    if (getAll) return res.status(200).json({ status: 200, vehicles: getAll });
+    const totalVehicles = await Vehicles.countDocuments({});
+    const maxpage = Math.ceil(totalVehicles / documentForPage);
+    const pageNumber = Number(req.query.page);
+
+    // Se non ci sono personaggi nella collection
+    if (totalVehicles === 0) {
+      return res.status(200).json({ message: "NO VEHICLES INTO COLLECTION" });
+    }
+
+    // Se non è settato il query params page
+    if (!pageNumber) {
+      urlNext = `http://localhost:${process.env.PORT}/v1/vehicles?page=2`;
+    }
+
+    // Se esiste il query params page ed è maggiore o uguale ad 1
+    if (pageNumber && pageNumber >= 1) {
+      urlNext = `http://localhost:${process.env.PORT}/v1/vehicles?page=${
+        pageNumber + 1
+      }`;
+      // Se esiste il query params page è maggiore o uguale ad 2
+      if (pageNumber >= 2) {
+        urlPrev = `http://localhost:${process.env.PORT}/v1/vehicles?page=${
+          pageNumber - 1
+        }`;
+      }
+    }
+
+    // Se il numero attuale di pagione e uguale al numero massimo di pagine
+    if (pageNumber === maxpage) {
+      urlNext = null;
+      // Se si prova a mettere un numero manualemente come query param di page
+    } else if (pageNumber > maxpage) {
+      return res.status(200).json({
+        message: "No more character",
+        urlPrev:
+          (urlPrev = `http://localhost:${process.env.PORT}/v1/vehicles?page=${maxpage}`),
+      });
+    }
+
+    // Se il numero massimo di pagine è 1 mette a null entrambe (MESSO ALLA FINE IN MODO DA SOVVRASCRIVERE LE ALTRE COSE)
+    if (maxpage === 1) {
+      urlPrev = null;
+      urlNext = null;
+    }
+
+    const getAll = await Vehicles.find(
+      req.query.name ? { name: req.query.name } : {}
+    )
+      .skip(
+        !pageNumber || pageNumber === 1 ? 0 : (pageNumber - 1) * documentForPage
+      )
+      .limit(documentForPage);
+    if (getAll)
+      return res
+        .status(200)
+        .json({
+          maxpage,
+          totalVehicles,
+          next: urlNext,
+          prev: urlPrev,
+          vehicles: getAll,
+        });
     return res
       .status(404)
       .json({ status: 404, message: "Not found vehicles list" });
--- src/controller/weapons.controller.ts
+++ src/controller/weapons.controller.ts
@@ -3,10 +3,74 @@ import Weapons from "../model/weapons.model";
 import Endpoint from "../model/endpoint.model";
 import IWeapons from "../interface/weapons.interface";
 
-export const getAllWeapons = async ({ query }: Request, res: Response) => {
+export const getAllWeapons = async (req: Request, res: Response) => {
+  //PAGINATION
+  let urlPrev = null;
+  let urlNext = null;
+  const documentForPage: number = 20;
   try {
-    const getAll = await Weapons.find(query);
-    if (getAll) return res.status(200).json({ status: 200, weapons: getAll });
+    const totalWeapons = await Weapons.countDocuments({});
+    const maxpage = Math.ceil(totalWeapons / documentForPage);
+    const pageNumber = Number(req.query.page);
+
+    // Se non ci sono personaggi nella collection
+    if (totalWeapons === 0) {
+      return res.status(200).json({ message: "NO WEAPONS INTO COLLECTION" });
+    }
+
+    // Se non è settato il query params page
+    if (!pageNumber) {
+      urlNext = `http://localhost:${process.env.PORT}/v1/weapons?page=2`;
+    }
+
+    // Se esiste il query params page ed è maggiore o uguale ad 1
+    if (pageNumber && pageNumber >= 1) {
+      urlNext = `http://localhost:${process.env.PORT}/v1/weapons?page=${
+        pageNumber + 1
+      }`;
+      // Se esiste il query params page è maggiore o uguale ad 2
+      if (pageNumber >= 2) {
+        urlPrev = `http://localhost:${process.env.PORT}/v1/weapons?page=${
+          pageNumber - 1
+        }`;
+      }
+    }
+
+    // Se il numero attuale di pagione e uguale al numero massimo di pagine
+    if (pageNumber === maxpage) {
+      urlNext = null;
+      // Se si prova a mettere un numero manualemente come query param di page
+    } else if (pageNumber > maxpage) {
+      return res.status(200).json({
+        message: "No more character",
+        urlPrev:
+          (urlPrev = `http://localhost:${process.env.PORT}/v1/weapons?page=${maxpage}`),
+      });
+    }
+
+    // Se il numero massimo di pagine è 1 mette a null entrambe (MESSO ALLA FINE IN MODO DA SOVVRASCRIVERE LE ALTRE COSE)
+    if (maxpage === 1) {
+      urlPrev = null;
+      urlNext = null;
+    }
+
+    const getAll = await Weapons.find(
+      req.query.name ? { name: req.query.name } : {}
+    )
+      .skip(
+        !pageNumber || pageNumber === 1 ? 0 : (pageNumber - 1) * documentForPage
+      )
+      .limit(documentForPage);
+    if (getAll)
+      return res
+        .status(200)
+        .json({
+          maxpage,
+          totalWeapons,
+          next: urlNext,
+          prev: urlPrev,
+          weapons: getAll,
+        });
     return res
       .status(404)
       .json({ status: 404, message: "Not found weapons list" });
