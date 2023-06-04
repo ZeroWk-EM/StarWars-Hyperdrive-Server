83a6c02179eaefdc29df5ca8e7d09be9c2b2168d
zerowk <em@zerowk.it>
2023-06-04 11:16:28 +0200

    feat(nodeMailer): add node mailer function

---
+++ src/config/nodeMailer.ts
@@ -0,0 +1,33 @@
+import nodeMailer from "nodemailer";
+
+const host = String(process.env.SMPT_HOST);
+const port = Number(process.env.SMPT_PORT);
+
+const user = String(process.env.SMPT_USER);
+const pass = String(process.env.SMPT_PASS);
+
+const EmailConnection = async (
+  email: string,
+  subject: string,
+  emailBody: string
+) => {
+  const transporter = nodeMailer.createTransport({
+    host,
+    port,
+    secure: true,
+    auth: {
+      user,
+      pass,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: user,
+    to: email,
+    subject,
+    html: emailBody,
+  });
+};
+
+
+export default EmailConnection;
