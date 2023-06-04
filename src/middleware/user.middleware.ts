6354393e50704735d2df657f3315bb68b2bc3305
zerowk <em@zerowk.it>
2023-06-04 11:23:32 +0200

    feat(user): define isAuth middleware

--- src/middleware/user.middleware.ts
+++ src/middleware/user.middleware.ts
@@ -1,5 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../model/user.model";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const key = String(process.env.SECRET_KEY);
 
 export const checkBodyValid = (
   req: Request,
@@ -16,4 +22,34 @@ export const checkBodyValid = (
     return res.status(400).json({ error_message: errorMsg });
   }
   next();
+};
+
+export const emailUnique = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(409).json({ message: "Email is just present" });
+  }
+  next();
+};
+
+export const isAuth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "Authorization token missing" });
+  }
+  try {
+    // Verify and decode the JWT token
+    const decodedToken = jwt.verify(token, key);
+    if (decodedToken) next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 };
