081f85c9473e0175979a7dd2a9f7c95584c38d37
zerowk <em@zerowk.it>
2023-06-04 11:15:17 +0200

    feat(auth): add middleware unique email

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { loginUser, registerUser } from "../controller/auth.controller";
+import { emailUnique } from "../middleware/user.middleware";
 
 const router = express.Router();
 router.use(express.json());
 
-router.post("/register", registerUser);
+router.post("/register", emailUnique, registerUser);
 
 router.post("/login", loginUser);
 
